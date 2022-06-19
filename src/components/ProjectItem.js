f1b027432b3878cb25bc13ae019d07af1291c5a6
Jeremy Platt <jplatt4427@gmail.com>
2022-06-19 12:32:26 -0700

    Completed Assignments

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  
+  const techElem = technologies.map((tech) => {
+    console.log(tech.name)
+    return <span key={tech}>{tech.name}</span>;
+  });
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      {techElem};
       </div>
     </div>
   );
